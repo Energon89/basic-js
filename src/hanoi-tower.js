6f58e9fb43132828655f81e6ee266f49fc14c6b4
Energon89 <podymov89@gmail.com>
2020-03-02 18:41:36 +0300

    feat: add calculateHanoi function

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,18 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+var turns = 0;
+
+function cauntTurns(n, A, B, C) {
+    if (n === 1) {
+        turns++;
+    } else {
+        cauntTurns(n - 1, A, C, B);
+        turns++;
+
+        cauntTurns(n - 1, C, B, A);
+    }
+}
+
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    cauntTurns(disksNumber, 0, 2, 1);
+    const seconds = turns / turnsSpeed * 3600;
+    return { turns, seconds };
 }
