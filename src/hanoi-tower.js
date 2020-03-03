a8c9b3bce9155aa2c6cfa7c6303a31fb7ea0abec
Energon89 <podymov89@gmail.com>
2020-03-03 13:23:34 +0300

    feat: fix hanoi-tower function

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,18 +1,19 @@
-var turns = 0;
+// var turns = 0;
 
-function cauntTurns(n, A, B, C) {
-    if (n === 1) {
-        turns++;
-    } else {
-        cauntTurns(n - 1, A, C, B);
-        turns++;
+// function cauntTurns(n, A, B, C) {
+//     if (n === 1) {
+//         turns++;
+//     } else {
+//         cauntTurns(n - 1, A, C, B);
+//         turns++;
 
-        cauntTurns(n - 1, C, B, A);
-    }
-}
+//         cauntTurns(n - 1, C, B, A);
+//     }
+// }
 
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-    cauntTurns(disksNumber, 0, 2, 1);
-    const seconds = turns / turnsSpeed * 3600;
+    // cauntTurns(disksNumber, 0, 2, 1);
+    const turns = Math.pow(2, disksNumber) - 1;
+    const seconds = (Math.pow(2, disksNumber) - 1) / (turnsSpeed / 3600);
     return { turns, seconds };
 }
